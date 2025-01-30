42f178f8373dd46bc4145689dbc55f828c966fad
Matheus Calmon <sanderhus15@outlook.com>
2025-01-30 16:51:13 -0300

    Add `Program.cs` to read and print user inputs.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entradada de dados pelo usuario tipo string  
+
+            string frase = Console.ReadLine();
+            
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
+
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Voce Digitou:");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine($"{a} {b} {c}");
+
+
+           
+
+        }
+    }
+}
