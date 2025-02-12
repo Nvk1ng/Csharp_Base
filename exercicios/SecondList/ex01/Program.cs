9ed11089fc2747a47794f54bdf372dd0e1925503
Matheus Calmon <sanderhus15@outlook.com>
2025-02-12 18:02:05 -0300

    Add new .NET 8.0 projects with various utility programs
    Added the following projects targeting .NET 8.0 with implicit usings and nullable enabled:
    - ConsoleApp4: Calculates roots of a quadratic equation.
    - ex01: Determines if an integer is negative.
    - ex02: Determines if an integer is even or odd.
    - ex03: Checks if two integers are multiples of each other.
    - ex04: Calculates the duration of a game given start and end times.
    - ex05: Calculates the total cost of an order based on item code and quantity.
    - ex06: Determines which interval a number falls into.
    - ex07: Determines the position of coordinates relative to the origin.
    - ex08: Calculates income tax based on salary.

---
+++ exercicios/SecondList/ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não. 
+
+            Console.WriteLine("Ditie um numero inteiro: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 >= 0)
+            {
+                Console.WriteLine("Inteiro!");
+            }
+            else
+            {
+                Console.WriteLine("Negativo");
+            }
+        }
+    }
+}
---
+++ exercicios/SecondList/ex02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar. 
+
+            Console.WriteLine("Digite um numero inteiro");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("É PAR!!");
+            }
+            else
+            {
+                Console.WriteLine("É IMPAR!!");
+            }
+
+        }
+    }
+}
