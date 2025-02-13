875544283eac756d4ceaea29204384dca5a78d44
Matheus Calmon <sanderhus15@outlook.com>
2025-02-13 17:40:58 -0300

    Add new projects ex01, ex02, ex03 with initial implementations
    Added project and solution files for ex01, ex02, and ex03, each targeting .NET 8.0 with implicit usings and nullable reference types enabled.
    
    - ex01: Implemented a password validation program in `Program.cs`.
    - ex02: Implemented a quadrant determination program in `Program.cs`.
    - ex03: Implemented a fuel type counting program in `Program.cs`.

---
+++ exercicios/ThirdList/ex01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.ConstrainedExecution;
+
+namespace ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+            incorreta informada, escrever a mensagem "Senha Invalida".Quando a senha for informada corretamente deve ser
+            impressa a mensagem "Acesso Permitido" e o algoritmo encerrado.Considere que a senha correta é o valor 2002.*/
+
+            Console.WriteLine("Digite a senha correta: ");
+
+            int password = int.Parse(Console.ReadLine());
+
+            while (password != 202)
+            {
+                Console.WriteLine("Senha invalida");
+                Console.WriteLine("Digite a senha correta: ");
+                password = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Acesso permitido");
+        }
+
+    }
+}
---
+++ exercicios/ThirdList/ex02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+                cartesiano.Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+                menos uma de duas coordenadas for NULA(nesta situação sem escrever mensagem alguma).*/
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+            while(x != 0 && y != 0)
+            {
+                if(x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro Quadrante");
+                }
+                else if(x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo quadrante");
+                }
+                else if(x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro quadrante");
+                }
+                else
+                {
+                    Console.WriteLine("Quarto Quadrante");
+                }
+
+                valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+            }
+
+        }
+    }
+}
