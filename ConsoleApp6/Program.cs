02d04c33af4bc65ece63f69097b5a8883b9cd9b2
Matheus Calmon <sanderhus15@outlook.com>
2025-02-18 20:00:17 -0300

    Implement basic console app in Program.cs to sum user-input integers.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quanto numeros inteiros voce vai digitar: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"valor #{i}: ");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            Console.WriteLine($"Soma igual: {soma}");
+        }
+    }
+}
