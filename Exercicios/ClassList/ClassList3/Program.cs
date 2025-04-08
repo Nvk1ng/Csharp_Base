499e8094bc5d06158537dd90362834125cc4d0e5
Matheus Calmon <sanderhus15@outlook.com>
2025-04-08 14:18:28 -0300

    Exercicios utilizando classes, atributos e metodos

---
+++ Exercicios/ClassList/ClassList3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassList3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retangulo: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("AREA: " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercicios/ClassList/ClassList3/Retangulo.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ClassList3{ 
+    class Retangulo{
+        
+        public double Largura;
+        public double Altura;
+
+        public double Area(){
+           return Largura * Altura;
+        }
+
+        public double Perimetro(){
+            return (Largura + Altura) * 2;
+        }
+
+        public double Diagonal(){
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+    
+}
