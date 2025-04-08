fc1b349a6e04c834292f52464f136b07550ff81a
Matheus Calmon <sanderhus15@outlook.com>
2025-04-08 15:06:15 -0300

    Exercicios utilizando classes, atributos e metodos

---
+++ Exercicios/ClassList/ClassList4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassList4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario f = new Funcionario();
+
+            Console.Write("Digite seu nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Digite seu Salario Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionario: " + f);
+
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+            double aumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(aumento);
+
+
+            Console.WriteLine("Dados Atualizados: " + f);
+        }
+    }
+}
