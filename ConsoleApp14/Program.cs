1688048cd90ec5c4b9a00e8b0d1575a5d2ceae18
Matheus Calmon <sanderhus15@outlook.com>
2025-04-10 11:33:23 -0300

    Exercicios utilizando auto properties

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp14{
+    class Program{
+        static void Main(string[] args){
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
