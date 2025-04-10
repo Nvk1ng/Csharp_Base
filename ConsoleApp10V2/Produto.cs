fb2e49ef9c32ebf13bf1262f8738e439c9d0c195
Matheus Calmon <sanderhus15@outlook.com>
2025-04-10 09:39:42 -0300

    Exercicios utilizando construtores

---
+++ ConsoleApp10V2/Produto.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Globalization;
+
+namespace ConsoleApp10V2{
+    class Produto{
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade ){
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque(){
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade){
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade){
+            Quantidade -= quantidade;
+        }
+        public override string ToString(){
+            return Nome 
+            + ", $ " 
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
